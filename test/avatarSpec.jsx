6cec107ef59deb761fdd5672da6cbaa1db298e9c
Artem Govorov <artem.govorov@gmail.com>
2015-11-13 00:09:06 +1000

    Updated tests

--- test/avatarSpec.jsx
+++ test/avatarSpec.jsx
@@ -2,27 +2,27 @@
 
 var testUtils = React.addons.TestUtils;
 
-describe('User Profile', () => {
+describe('User Profile', function () {
 
   var instance;
 
-  beforeEach(() => {
+  beforeEach(function () {
     this.container = document.createElement('div');
     document.body.appendChild(this.container);
     instance = React.render(<UserProfile username='Homer' />, this.container);
   });
 
-  afterEach(() => {
+  afterEach(function () {
     document.body.removeChild(this.container);
   });
 
-  describe('Follow button', () => {
+  describe('Follow button', function () {
 
-    it('should display user name', () => {
+    it('should display user name', function () {
       expect(instance.getDOMNode().textContent).toBe('Follow Homer');
     });
 
-    it('should change its text to "Unfollow" when clicked', () => {
+    it('should change its text to "Unfollow" when clicked', function () {
       testUtils.Simulate.click(testUtils.findRenderedDOMComponentWithTag(instance, 'p'));
       expect(instance.getDOMNode().textContent).toBe('Unfollow');
     });
