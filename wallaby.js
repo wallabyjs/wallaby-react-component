b8154a811efa14b6103b52aeb2b44af08761acb1
Artem Govorov <artem.govorov@gmail.com>
2016-08-31 22:02:55 +1000

    Update wallaby.js

--- wallaby.js
+++ wallaby.js
@@ -2,7 +2,6 @@ module.exports = function (wallaby) {
   return {
 
     files: [
-      {pattern: 'node_modules/phantomjs-polyfill/bind-polyfill.js', instrument: false},
       {pattern: 'node_modules/react/dist/react-with-addons.js', instrument: false},
       'assets/**',
       'src/**'
